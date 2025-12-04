eb362b5d63f3b915007f4e684ae4e6bdd478855d
Chuong <vanchuong060704@gmail.com>
2025-12-04 09:38:54 +0700

    Initial project

---
+++ src/main/java/com/friendhub/FriendhubApplication.java
@@ -0,0 +1,13 @@
+package com.friendhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/friendhub/FriendhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

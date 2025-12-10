c6f4beb731869add78339115c40004cc4f91e3cc
Chuong <vanchuong060704@gmail.com>
2025-12-10 11:17:40 +0700

    feat: update UserRepository and PostRepository

--- src/main/java/com/friendhub/repository/UserRepository.java
+++ src/main/java/com/friendhub/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.friendhub.repository;
 
 import com.friendhub.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,5 +12,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT u.* " +
+            "FROM users u " +
+            "LEFT JOIN friends f " +
+            "ON (f.requester_id = u.id AND f.addressee_id = :userId) " +
+            "OR (f.addressee_id = u.id AND f.requester_id = :userId) " +
+            "WHERE u.id != :userId " +
+            "AND (f.id IS NULL " +
+            "OR f.status NOT IN ('ACCEPTED', 'PENDING', 'BLOCKED')) ", nativeQuery = true)
+    List<User> findAllPotentialFriends(long userId);
 
 }

2bad32201cb4b8c8a992f245e3ab77a18f896be6
Chuong <vanchuong060704@gmail.com>
2025-12-10 13:21:30 +0700

    feat: add jwtAuthenticationConverter

--- src/main/java/com/friendhub/config/ApplicationInitConfig.java
+++ src/main/java/com/friendhub/config/ApplicationInitConfig.java
@@ -3,6 +3,8 @@ package com.friendhub.config;
 import com.friendhub.entity.Role;
 import com.friendhub.entity.User;
 import com.friendhub.enums.ERole;
+import com.friendhub.enums.ErrorCode;
+import com.friendhub.exception.AppException;
 import com.friendhub.repository.RoleRepository;
 import com.friendhub.repository.UserRepository;
 import org.springframework.boot.ApplicationRunner;
@@ -26,10 +28,11 @@ public class ApplicationInitConfig {
             if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
 
                 Role role = roleRepository.findByName(ERole.ADMIN)
-                        .orElseThrow(() -> new RuntimeException("Role not found"));
+                        .orElseThrow(() -> new AppException(ErrorCode.ROLE_NOT_FOUND));
 
                 User user = User.builder()
                         .firstName("admin")
+                        .lastName("")
                         .email("admin@gmail.com")
                         .password(passwordEncoder.encode("123456"))
                         .role(role)

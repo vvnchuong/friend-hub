ecf1c11174fdbe30be967148ff14027a3c24384f
Chuong <vanchuong060704@gmail.com>
2025-12-19 14:19:22 +0700

    feat: add MediaType and UnFriendRequest

---
+++ src/main/java/com/friendhub/dto/request/UnFriendRequest.java
@@ -0,0 +1,19 @@
+package com.friendhub.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UnFriendRequest {
+
+    @NotNull(message = "User ID is required")
+    long userId;
+
+}
---
+++ src/main/java/com/friendhub/enums/MediaType.java
@@ -0,0 +1,8 @@
+package com.friendhub.enums;
+
+public enum MediaType {
+
+    IMAGE,
+    VIDEO
+
+}

f1a4f79216b756a81cdd1b99fedefb36fc6c5388
Chuong <vanchuong060704@gmail.com>
2025-12-19 09:45:51 +0700

    feat: add FriendController

---
+++ src/main/java/com/friendhub/dto/request/CommentCreationRequest.java
@@ -0,0 +1,20 @@
+package com.friendhub.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CommentCreationRequest {
+
+    @NotBlank(message = "Content is required")
+    String content;
+
+}
---
+++ src/main/java/com/friendhub/dto/response/CommentResponse.java
@@ -0,0 +1,24 @@
+package com.friendhub.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CommentResponse {
+
+    String content;
+    long postId;
+    UserResponse author;
+    Instant createdAt;
+
+}

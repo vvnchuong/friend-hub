a3cac4d26286e52ef6b5cad7b2299d228d74ff85
Chuong <vanchuong060704@gmail.com>
2025-12-19 09:43:57 +0700

    feat: add FriendController

---
+++ src/main/java/com/friendhub/controller/FriendController.java
@@ -0,0 +1,82 @@
+package com.friendhub.controller;
+
+import com.friendhub.dto.request.FriendAcceptRequest;
+import com.friendhub.dto.request.FriendCancelRequest;
+import com.friendhub.dto.request.FriendCreationRequest;
+import com.friendhub.dto.request.UnFriendRequest;
+import com.friendhub.dto.response.ApiResponse;
+import com.friendhub.dto.response.FriendResponse;
+import com.friendhub.service.FriendService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/friends")
+public class FriendController {
+
+    private final FriendService friendService;
+
+    @PostMapping("/requests")
+    public ApiResponse<String> addFriend(@RequestBody FriendCreationRequest request) {
+        friendService.addFriendRequest(request);
+
+        return ApiResponse.<String>builder()
+                .message("Friend request sent successfully.")
+                .build();
+    }
+
+    @PostMapping("/accept")
+    public ApiResponse<String> acceptFriendRequest(@RequestBody FriendAcceptRequest request) {
+        friendService.acceptFriendRequest(request);
+
+        return ApiResponse.<String>builder()
+                .message("Friend request accepted successfully.")
+                .build();
+    }
+
+    @PostMapping("/cancel")
+    public ApiResponse<String> rejectFriendRequest(@RequestBody FriendCancelRequest request) {
+        friendService.rejectFriendRequest(request);
+
+        return ApiResponse.<String>builder()
+                .message("Friend request canceled successfully.")
+                .build();
+    }
+
+    @PostMapping("/unfriend")
+    public ApiResponse<String> unFriend(@RequestBody UnFriendRequest request) {
+        friendService.unFriend(request);
+
+        return ApiResponse.<String>builder()
+                .message("Unfriended successfully.")
+                .build();
+    }
+
+    @GetMapping
+    public ApiResponse<List<FriendResponse>> getAllFriendsByUser() {
+        return ApiResponse.<List<FriendResponse>>builder()
+                .result(friendService.getAllFriendsByUser())
+                .message("Friend request canceled successfully.")
+                .build();
+    }
+
+    @GetMapping("/requests")
+    public ApiResponse<List<FriendResponse>> getAllFriendRequests() {
+        return ApiResponse.<List<FriendResponse>>builder()
+                .message("Friends retrieved successfully.")
+                .result(friendService.getAllFriendRequestsByUser())
+                .build();
+    }
+
+    @GetMapping("/potential")
+    public ApiResponse<List<FriendResponse>> getAllPotentialFriends() {
+        return ApiResponse.<List<FriendResponse>>builder()
+                .message("Friends retrieved successfully.")
+                .result(friendService.getAllPotentialFriends())
+                .build();
+    }
+
+}

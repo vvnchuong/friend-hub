c52c92b1c6d04352f7a33f7df8d0983a136d06e3
Chuong <vanchuong060704@gmail.com>
2025-12-26 08:53:30 +0700

    fix: add missing PageResponse

---
+++ src/main/java/com/friendhub/dto/response/PageResponse.java
@@ -0,0 +1,23 @@
+package com.friendhub.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageResponse<T> {
+
+    List<T> content;
+    int page;
+    int size;
+    long totalElements;
+    int totalPages;
+    boolean first;
+    boolean last;
+
+}

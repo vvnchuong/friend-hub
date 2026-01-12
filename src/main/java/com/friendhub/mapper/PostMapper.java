2def654a2876ea06045dae8c438938531293ae40
Chuong <vanchuong060704@gmail.com>
2026-01-12 16:44:24 +0700

    feat: add originalPost mapper

--- src/main/java/com/friendhub/mapper/PostMapper.java
+++ src/main/java/com/friendhub/mapper/PostMapper.java
@@ -3,35 +3,21 @@ package com.friendhub.mapper;
 import com.friendhub.dto.request.PostCreationRequest;
 import com.friendhub.dto.request.PostUpdateRequest;
 import com.friendhub.dto.response.PostResponse;
-import com.friendhub.entity.Group;
 import com.friendhub.entity.Post;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
-import java.time.Instant;
-
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+        uses = UserMapper.class)
 public interface PostMapper {
 
     Post toPost(PostCreationRequest request);
 
     @Mapping(target = "author", source = "user")
-    @Mapping(target = "author.role", source = "user.role.name")
+    @Mapping(target = "originalPost.author", source = "originalPost.user")
     PostResponse toPostResponse(Post post);
 
     void updatePost(@MappingTarget Post post, PostUpdateRequest request);
-
-    @AfterMapping
-    default void setCreatedAt(@MappingTarget Post post) {
-        if (post.getCreatedAt() == null)
-            post.setCreatedAt(Instant.now());
-    }
-
-    @AfterMapping
-    default void setUpdatedAt(@MappingTarget Post post) {
-        post.setUpdatedAt(Instant.now());
-    }
 
 }

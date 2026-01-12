5a488d1ff6f14dcf9702a8350cc528b66e6f28b2
Chuong <vanchuong060704@gmail.com>
2026-01-12 16:42:14 +0700

    feat: add PostSharedResponse

--- src/main/java/com/friendhub/dto/response/PostSharedResponse.java
+++ src/main/java/com/friendhub/dto/response/PostSharedResponse.java
@@ -8,25 +8,17 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.time.Instant;
-import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class PostResponse {
+public class PostSharedResponse {
 
     long id;
     String content;
-    List<PostMediaResponse> mediaList;
-    UserBasicInfoResponse author;
-    PostSharedResponse originalPost;
-    Instant createdAt;
-    Instant updatedAt;
     Privacy privacy;
-    int totalLikes;
-    int totalComments;
-    boolean isLiked;
-    GroupResponse group;
+    Instant createdAt;
+    UserBasicInfoResponse author;
 
 }

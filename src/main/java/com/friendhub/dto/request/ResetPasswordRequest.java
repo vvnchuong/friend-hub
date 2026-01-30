b7b87b5f056f4bc91fc9e6460ec7ff90f78b9222
Chuong <vanchuong060704@gmail.com>
2026-01-30 14:36:44 +0700

    feat: add ForgetPassword and ResetPassword requests

---
+++ src/main/java/com/friendhub/dto/request/ResetPasswordRequest.java
@@ -0,0 +1,19 @@
+package com.friendhub.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResetPasswordRequest {
+
+    String token;
+    String newPassword;
+    String confirmPassword;
+
+}

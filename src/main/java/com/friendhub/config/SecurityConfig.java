1f5f7bee42ad63b391f2fdc967ef2d93668884c9
Chuong <vanchuong060704@gmail.com>
2026-01-31 11:12:32 +0700

    refactor: clean up SecurityConfig and group endpoint rules

--- src/main/java/com/friendhub/config/SecurityConfig.java
+++ src/main/java/com/friendhub/config/SecurityConfig.java
@@ -1,28 +1,93 @@
 package com.friendhub.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final CustomJwtDecoder customJwtDecoder;
+
+    private final String[] PUBLIC_POST_ENDPOINTS = {"/users", "/auth/**"};
+
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/uploads/**",
+            "/chats/**",
+            "/ws/**"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request ->
-                        request.anyRequest().permitAll());
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
+                        // public
+                        .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers(HttpMethod.POST, PUBLIC_POST_ENDPOINTS).permitAll()
+
+                        // member + admin
+                        .requestMatchers(
+                                HttpMethod.POST,
+                                "/posts",
+                                "/files",
+                                "/friends",
+                                "/groups",
+                                "/reports"
+                        ).hasAnyRole("ADMIN", "MEMBER")
+                        .requestMatchers(HttpMethod.GET, "/users").hasAnyRole("ADMIN", "MEMBER")
+
+                        // admin only
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        // everything else
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(o2 ->
+                        o2.jwt(jwtConfigurer ->
+                                jwtConfigurer.decoder(customJwtDecoder)
+                                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://localhost:5173");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter(){
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 
     @Bean
